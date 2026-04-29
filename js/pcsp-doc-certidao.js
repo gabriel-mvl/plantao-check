457dfb9932d7a327180f54135ddc2d694b548c8c
gabriel-mvl <gabriel.martinsv@gmail.com>
2026-04-29 12:41:09 -0300

    Update pcsp-doc-certidao.js

--- js/pcsp-doc-certidao.js
+++ js/pcsp-doc-certidao.js
@@ -20,6 +20,7 @@ PCSP_DOCS.certidao = {
       ],
     },
     { id: 'nomeEnvolvido',  label: 'Nome do(a) indiciado(a) / requerido(a)', placeholder: 'Nome completo', required: false },
+    { id: 'numBO',          label: 'Número do BO', placeholder: 'Ex: 000001/2026', required: false },
     { id: 'numProcesso',    label: 'N\u00famero do processo (medida protetiva)', placeholder: 'Ex: 1234567-89.2026.8.26.0000', required: false },
     { id: 'textoLivre',     label: 'Texto livre da certid\u00e3o', placeholder: 'Descreva o conte\u00fado da certid\u00e3o...', type: 'textarea', required: false },
     { id: 'nomeEscrivao',   label: 'Nome completo do(a) escrivão(ã)', placeholder: 'Nome completo' },
@@ -30,6 +31,7 @@ PCSP_DOCS.certidao = {
     var J   = S + 'text-align:justify;line-height:1.8;';
     var nome = campos.nomeEnvolvido || '___________________________________';
     var num  = campos.numProcesso   || '___________________________________';
+    var numBO = campos.numBO ? 'número ' + campos.numBO : '_______________';
     var tipo = campos.tipo;
 
     // Body text
@@ -37,23 +39,23 @@ PCSP_DOCS.certidao = {
     switch (tipo) {
       case 'recusa_interr':
         corpo_texto =
-          'o(a) indiciado(a) <strong>' + nome + '</strong>, devidamente qualificado(a) nos autos, ' +
+          'o(a) indiciado(a) <strong>' + nome + '</strong>, devidamente qualificado(a) nos autos do boletim de ocorr\u00eancia ' + numBO + ', ' +
           'foi cientificado(a) do teor de seu interrogat\u00f3rio e da respectiva nota de culpa, ' +
           'recusando-se a assinar ambos os documentos, o que fa\u00e7o constar para os devidos fins legais.';
         break;
 
       case 'recusa_dactilo':
         corpo_texto =
-          'o(a) indiciado(a) <strong>' + nome + '</strong>, devidamente qualificado(a) nos autos, ' +
+          'o(a) indiciado(a) <strong>' + nome + '</strong>, devidamente qualificado(a) nos autos do boletim de ocorr\u00eancia ' + numBO + ', ' +
           'foi cientificado(a) do teor de seu interrogat\u00f3rio e da respectiva nota de culpa, ' +
           'recusando-se a assinar ambos os documentos, bem como n\u00e3o permitiu a coleta de suas ' +
-          'impress\u00f5es datiloscópicas para fins de legitima\u00e7\u00e3o dos atos praticados, ' +
+          'impress\u00f5es datiloscópicas para fins de legitima\u00e7\u00e3o, ' +
           'o que fa\u00e7o constar para os devidos fins legais.';
         break;
 
       case 'recusa_tco':
         corpo_texto =
-          'o(a) autor(a) do fato <strong>' + nome + '</strong>, devidamente qualificado(a) nos autos, ' +
+          'o(a) autor(a) do fato <strong>' + nome + '</strong>, devidamente qualificado(a) nos autos do boletim de ocorr\u00eancia ' + numBO + ', ' +
           'foi cientificado(a) do teor do Termo Circunstanciado de Ocorr\u00eancia lavrado, ' +
           'recusando-se a assin\u00e1-lo, o que fa\u00e7o constar para os devidos fins legais.';
         break;
